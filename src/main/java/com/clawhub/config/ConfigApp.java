5bc4b53b1418a86f1144ef29e4b7d72ec0fcb37f
李志明 <18120177709@163.com>
2018-09-06 22:51:19 +0800

    后台跨域处理

--- src/main/java/com/clawhub/config/ConfigApp.java
+++ src/main/java/com/clawhub/config/ConfigApp.java
@@ -4,6 +4,9 @@ import com.clawhub.spring.SpringContextHelper;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * <Description> 配置<br>
@@ -23,5 +26,19 @@ public class ConfigApp {
     @Bean
     public SpringContextHelper springHelper() {
         return new SpringContextHelper();
+    }
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 }
